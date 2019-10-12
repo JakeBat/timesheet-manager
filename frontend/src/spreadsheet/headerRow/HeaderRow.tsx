e8883cc5418a2052e617bca4838f5ccf9e63276f
JakeBat <jake_batchelor@hotmail.com>
2019-10-12 13:23:54 -0600

    index on master: 07b4479 init commit

---
+++ frontend/src/spreadsheet/headerRow/HeaderRow.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TableHeader.css';
+
+const tableHeader = () => {
+    return(
+        <div></div>
+    )
+};
+
+export default tableHeader;
---
+++ frontend/src/spreadsheet/tableRow/TableRow.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TableRow.css.css';
+
+const tableRow = () => {
+    return (
+        <div></div>
+    )
+};
+
+export default tableRow;
